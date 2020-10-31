a052acf30ad24614791fc10488a6fd5c1724247f
asabdulkareem <as.abdulkareem@gmail.com>
2020-10-31 19:17:04 +0530

    Simplifying the Speaker Components

--- pages/speakers.js
+++ pages/speakers.js
@@ -1,37 +1,10 @@
-import React from 'react';
-import Header from '../src/components/Header/Header';
-import Menu from '../src/components/Menu/Menu';
-import SearchBar from '../src/components/SearchBar/SearchBar';
 import Speakers from '../src/components/Speakers/Speakers';
-import Footer from '../src/components/Footer/Footer';
-import SpeakerContext from '../src/components/Speakers/SpeakerContext';
+import Layout from '../src/components/Layout/Layout';
 
-function Page(){
-    const speakers = [
-        {
-        imageSrc: 'speaker-component-1124',
-        name: 'Douglas Crockford',
-        },
-        {
-        imageSrc: 'speaker-component-1530',
-        name: 'Tamara Baker',
-        },
-        {
-        imageSrc: 'speaker-component-10803',
-        name: 'Eugene Chuvyrov',
-        },
-    ];
-    return(
-        <div>
-            <Header />
-            <Menu />
-            <SpeakerContext.Provider value={speakers}>
-                <SearchBar />
-                <Speakers />
-            </SpeakerContext.Provider>
-            <Footer />
-        </div>
-    );
+export default function Page() {
+  return (
+    <Layout>
+      <Speakers />
+    </Layout>
+  );
 }
-
-export default Page; 
